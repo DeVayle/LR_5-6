c139a042560b83c478acc464aae964b3edd0e981
DeVayle <144678017+DeVayle@users.noreply.github.com>
2023-10-26 13:11:24 +0300

    lr56_10.pas

--- lr56_10.pas
+++ lr56_10.pas
@@ -1,24 +1,36 @@
-﻿program lr56_10;
+program lr56_10;
 
 const
-  n = 20;
+m = 1000;
 
 var
-  i: integer;
-  a: array[1..n] of integer;
+i, j, n: integer;
+a: array[1..m] of integer;
 
 begin
-  writeln('Изначальный массив:');
-  for i := 1 to n do
-  begin
-    a[i] := random(-99, 99); 
-    write(A[i], ' ');
-  end;
-writeln;
-writeln ('Измененный массив без отрицательных чисел:');
-i:=1;
-while i <= n do
+n := 20;
+writeln('Изначальный массив:');
+for i := 1 to n do
 begin
+a[i] := random(-99, 99);
+write(A[i], ' ');
+end;
+writeln;
+writeln('Измененный массив без отрицательных чисел:');
+
+i := 1;
+while i <= n do
+if a[i] < 0 then
+begin
+for j := i to n - 1 do
+a[j] := a[j + 1];
+dec(n);
+end
+else i := i + 1;
+
+for i := 1 to n do
+write(a[i], ' ');
+end.
   if a[i] >= 0 then
   write (a[i], ' ');
   inc(i);
